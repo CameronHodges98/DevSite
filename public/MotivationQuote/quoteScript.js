f3d349ee6d65c0eca927d2b46375e8d4ba0b5864
Cameron Hodges <chodges98@gmail.com>
2026-04-13 18:19:34 -0700

    added 7 more quotes to the generator program

--- public/MotivationQuote/quoteScript.js
+++ public/MotivationQuote/quoteScript.js
@@ -2,10 +2,17 @@
 const quotes = [
   "The only way to do great work is to love what you do.",
   "The future belongs to those who believe in the beauty of their dreams.",
-  "Don't watch the clock; do what it does. Keep going.",
+  "Don’t watch the clock; do what it does. Keep going.",
   "Success is not final, failure is not fatal: It is the courage to continue that counts.",
   "Your time is limited, so don’t waste it living someone else’s life.",
-  "The best way to predict the future is to create it."
+  "The best way to predict the future is to create it.",
+  "It does not matter how slowly you go as long as you do not stop.",
+  "Believe you can and you’re halfway there.",
+  "Hard work beats talent when talent doesn’t work hard.",
+  "You miss 100% of the shots you don’t take.",
+  "I am not a product of my circumstances. I am a product of my decisions.",
+  "The man who has confidence in himself gains the confidence of others.",
+  "Start where you are. Use what you have. Do what you can."
 ];
 
 const people = [
@@ -14,7 +21,14 @@ const people = [
   "Sam Levenson",
   "Winston Churchill",
   "Steve Jobs",
-  "Peter Drucker"
+  "Peter Drucker",
+  "Confucius",
+  "Theodore Roosevelt",
+  "Tim Notke",
+  "Wayne Gretzky",
+  "Stephen Covey",
+  "Hasidic Proverb",
+  "Arthur Ashe"
 ];
 
 let previousIndex = -1;
