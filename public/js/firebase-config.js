d545b06bd843eecc8403bc24aa30eaae4afd9c35
Cameron Hodges <chodges98@gmail.com>
2026-04-14 15:38:52 -0700

    Security Update

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 
 // Get these values from Firebase Console → Project Settings → Your apps → Web app config
 const firebaseConfig = {
-  apiKey: "AIzaSyDeEBHLXJ2YLSkeU8W-cPh7KnFKjBoZ4q0",
-  authDomain: "camdevsite.firebaseapp.com",
-  projectId: "camdevsite",
-  storageBucket: "camdevsite.firebasestorage.app",
-  messagingSenderId: "977617478668",
-  appId: "1:977617478668:web:6784cc787e7947c4dd3a29"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
